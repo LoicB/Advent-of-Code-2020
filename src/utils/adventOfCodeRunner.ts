72b1b9d6cad8cfcd6366f62dca3e9099ef22a7a5
Loic Beylot <loic.beylot@ricoh-europe.com>
2020-12-09 18:33:19 +0000

    Change timer name for each parts

--- src/utils/adventOfCodeRunner.ts
+++ src/utils/adventOfCodeRunner.ts
@@ -1,11 +1,11 @@
 export function runPartOneAndTwo<T>(partOne: (input: T) => number | undefined, partTwo: (input: T) => number | undefined, input: T) {
     if (process.env.npm_lifecycle_event && !process.env.npm_lifecycle_event.startsWith('test')) {
-        console.time('Time')
+        console.time('Time to part 1')
         const resultA = partOne(input)
-        console.timeEnd('Time')
-        console.time('Time')
+        console.timeEnd('Time to part 1')
+        console.time('Time to part 2')
         const resultB = partTwo(input)
-        console.timeEnd('Time')
+        console.timeEnd('Time to part 2')
 
         console.log('Solution to part 1:', resultA)
         console.log('Solution to part 2:', resultB)
